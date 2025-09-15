11ed8cf6965e9ddafa7da8df7f10938c39fe1cbb
Deva Harsha Sai Nangunuri <21ag1a66a4@gmail.com>
2025-09-15 20:44:58 +0530

    Delete sortingTechniques directory

--- sortingTechniques/bubbleSort.py
+++
@@ -1,8 +0,0 @@
-ls = list(map(int,input().split()))
-for _ in range(len(ls)):
-    i = 0
-    while(i<len(ls)-1):
-        if ls[i]>=ls[i+1]:
-            ls[i],ls[i+1] = ls[i+1],ls[i]
-        i+=1
-print(ls)
